a0455eecb66cbf8bc1fe3c27afefdd6c95a50f3a
SH20RAJ <66713844+SH20RAJ@users.noreply.github.com>
2023-06-29 05:37:56 +0000

    Adding Aptoide

--- api/test.php
+++ api/test.php
@@ -1,11 +1,23 @@
 <?php
-$url = 'https://example.com/';  // Replace with the URL of your JSON file
+$url = 'https://appspages.sh20raj.com/api/aptoide.php';  // Replace with the URL of your JSON file
 
 
 // Fetch the JSON data
-echo $jsonData = file_get_contents($url);
+ $jsonData = file_get_contents($url);
 
 // Decode the JSON data
-//echo $data = json_decode($jsonData, true);  // Set the second parameter to 'true' for associative array
+ $data = json_decode($jsonData, true);  // Set the second parameter to 'true' for associative array
+$data = $data['props']['pageProps'];
 
-?>
+$topAppsBundle = $data['topAppsBundle']['list'];
+
+
+// Iterate through each app and display the details
+foreach ($topAppsBundle as $app) {
+    echo "Icon: <img src=\"" . $app['icon'] . "\" alt=\"" . $app['name'] . "\" width=\"50\" height=\"50\"><br>";
+    echo "Name: " . $app['name'] . "<br>";
+    echo "Score: " . $app['score'] . "<br>";
+    echo "Size: " . $app['size'] . "<br>";
+    echo "Store: " . $app['store'] . "<br>";
+    echo "Package: " . $app['package'] . "<br><br>";
+}
