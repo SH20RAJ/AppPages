448b263b14250d6f276240c92fbd598b2a4c4021
SH20RAJ <66713844+SH20RAJ@users.noreply.github.com>
2023-06-30 13:00:04 +0000

    canaconic set

--- assets/head.php
+++ assets/head.php
@@ -3,8 +3,8 @@
     <meta charset="utf-8" />
     <title>AppsPages : Make Your App's Page Online</title>
     <meta name="description" content="Free Games and Programs, you can download for free, a lot popular games with MOD for Android." />
-    <meta name="keywords" content="programs for android, games for android, android1, android 1, android 1 com, mod games, android, mod apk, apk" />
-    <link rel="search" type="application/opensearchdescription+xml" href="https://an1.com/engine/opensearch.php" title="News, free games and program for Android" />
+    <meta name="keywords" content="AppsPages, app downloading, website, APKPure, download, Android, iOS, applications, software, games, utilities, free apps, premium apps, app repository, mobile apps, app store, app market, APK files, app reviews, app ratings, app updates, app categories, app recommendations,Apps, Pages, App Store, Download, APK, Android, iOS, Windows, Mac, Games, Utilities, Tools, Productivity, Entertainment, Communication, Social Networking, Media, Music, Video, Photos, Education, Health, Fitness, Lifestyle, News, Weather, Sports, Shopping, Finance, Travel, Navigation, Food, Drink, Books, Reference, Art, Design, Photography, Personalization, Business, Professional, Development, Parenting, Kids, Family, Dating, Relationships, Gaming, Adventure, Strategy, Puzzle, Simulation, Arcade, Casual, Racing, Sports, Multiplayer, Free, Paid, Popular, Trending, Latest, Top-rated, Reviews" />
+    <link rel="search" type="application/opensearchdescription+xml" href="https://appspages.online/sitemaps/opensearch.php" title="News, free games and program for Android" />
     <link rel="alternate" hreflang="x-default" href="" />
     <link rel="alternate" hreflang="en" href="" />
     <link rel="alternate" hreflang="ru" href="" />
@@ -25,11 +25,11 @@
     <link rel="icon" type="image/png" sizes="32x32" href="https://appspages.online/logo.png" />
     <link rel="icon" type="image/png" sizes="16x16" href="https://appspages.online/logo.png" />
     <meta name="theme-color" content="#ffffff" />
-    <meta name="msapplication-config" content="https://an1.com/templates/an1/images/favicon/browserconfig.xml" />
+    <!--<meta name="msapplication-config" content="https://an1.com/templates/an1/images/favicon/browserconfig.xml" />
     <link rel="manifest" href="https://an1.com/templates/an1/images/favicon/manifest.json" />
     <link rel="mask-icon" href="https://an1.com/templates/an1/images/favicon/safari-pinned-tab.svg" color="#5bbad5" />
     <link rel="preload" href="https://an1.com/templates/an1/fonts/montserrat-700.woff2" as="font" type="font/woff2" crossorigin="" />
-    <link rel="preload" href="https://an1.com/templates/an1/fonts/montserrat-400.woff2" as="font" type="font/woff2" crossorigin="" />
+    <link rel="preload" href="https://an1.com/templates/an1/fonts/montserrat-400.woff2" as="font" type="font/woff2" crossorigin="" />-->
     <link rel="preload" href="https://an1.com/templates/an1/css/styles.css?v=6.0" as="style" />
     <link href="https://an1.com/templates/an1/css/styles.css?v=6.0" type="text/css" rel="stylesheet" />
     <script async="" src="https://cdn.jsdelivr.net/npm/yandex-metrica-watch/tag.js"></script>
---
+++ sitemaps/opensearch.php
@@ -0,0 +1,18 @@
+
+<?xml version="1.0" encoding="utf-8"?>
+<OpenSearchDescription xmlns="http://a9.com/-/spec/opensearch/1.1/">
+	<ShortName>AppsPages.online</ShortName>
+	<Description>AppsPages.online Search</Description>
+	<Contact>mail@appspages.online</Contact>
+	<Url type="text/html" template="https://an1.comsearch/{searchTerms}&amp;do=search&amp;subaction=search" />
+	<LongName>AppsPages.online Search</LongName>
+	<Image height="32" width="32" type="image/png">https://appspages.online/logo.png</Image>
+	<Image height="32" width="32" type="image/vnd.microsoft.icon">https://appspages.online/logo.png</Image>
+	<Developer>AppsPages.online</Developer>
+	<Attribution>2023. (C) AppsPages.online - Games, programs for android</Attribution>
+	<SyndicationRight>open</SyndicationRight>
+	<AdultContent>false</AdultContent>
+	<Language>en-us</Language>
+	<OutputEncoding>utf-8</OutputEncoding>
+	<InputEncoding>utf-8</InputEncoding>
+</OpenSearchDescription>
