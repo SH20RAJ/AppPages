6e2b533febd3169616ff86d805ba80a6f93d69a8
SH20RAJ <66713844+SH20RAJ@users.noreply.github.com>
2023-06-30 06:45:43 +0000

    partition

---
+++ assets/footer.php
@@ -0,0 +1,48 @@
+    <footer class="footer">
+      <div class="wrp">
+        <div class="copyright smf">
+          <a href="/" aria-label="AP">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="15.1" viewBox="0 0 16 15.1">
+              <path fill="#54B247" d="M9.5,0h-6L10,15.1h6L9.5,0z"></path>
+              <path fill="#68CB5B" d="M6,8l3.5-8h-6L0,8H6z"></path>
+            </svg>
+          </a>
+          <span class="copyright_i1 fbold">2023. © AppsPages.online</span>
+          <span class="copyright_i2">Games, programs for android</span>
+        </div>
+      </div>
+      <div class="foot_line xsmf">
+        <div class="wrp">
+          <ul class="foot_menu">
+            <li>
+              <a href="/dmca.html">DMCA Policy</a>
+            </li>
+            <li>
+              <a href="/privacy-policy.html">Privacy Policy</a>
+            </li>
+            <li>
+              <a href="/index.php?do=feedback">Feedback</a>
+            </li>
+          </ul>
+          <div class="social" title="Read Us">
+            <a class="soc_tg" href="https://t.me/android1com" rel="noopener nofollow" target="_blank">
+              <svg class="i__tg" width="32" height="32">
+                <use xlink:href="#i__tg"></use>
+              </svg>
+            </a>
+            <a class="soc_fb" href="#" rel="noopener nofollow" target="_blank">
+              <svg class="i__fb" width="32" height="32">
+                <use xlink:href="#i__fb"></use>
+              </svg>
+            </a>
+            <a class="soc_yt" href="https://www.youtube.com/cxdiin" rel="noopener nofollow" target="_blank">
+              <svg class="i__yt" width="32" height="32">
+                <use xlink:href="#i__yt"></use>
+              </svg>
+            </a>
+          </div>
+        </div>
+      </div>
+    </footer>
+    
+    <script src="index.js"></script>
