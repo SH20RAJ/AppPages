772f9d60f238cdacd9e29e2dae24182cef0c9902
SH20RAJ <66713844+SH20RAJ@users.noreply.github.com>
2023-06-30 12:47:33 +0000

    meta

--- assets/head.php
+++ assets/head.php
@@ -1,20 +1,20 @@
 <head>
     <meta name="google-site-verification" content="dnuxRFPlXTc47CbbYyiaq8eTi_hQZ5StdvZMw6fLm9A" />
     <meta charset="utf-8" />
-    <title>AppsPages</title>
+    <title>AppsPages : Make Your App's Page Online</title>
     <meta name="description" content="Free Games and Programs, you can download for free, a lot popular games with MOD for Android." />
     <meta name="keywords" content="programs for android, games for android, android1, android 1, android 1 com, mod games, android, mod apk, apk" />
     <link rel="search" type="application/opensearchdescription+xml" href="https://an1.com/engine/opensearch.php" title="News, free games and program for Android" />
     <link rel="alternate" hreflang="x-default" href="" />
     <link rel="alternate" hreflang="en" href="" />
     <link rel="alternate" hreflang="ru" href="" />
-    <meta property="og:site_name" content="AN1.com" />
+    <meta property="og:site_name" content="AppsPages.online" />
     <meta property="og:type" content="website" />
-    <meta property="og:url" content="" />
+    <meta property="og:url" content="https://appspages.online/" />
     <meta property="og:image" content="https://appspages.online/logo.png" />
     <meta property="og:image:width" content="180" />
     <meta property="og:image:height" content="180" />
-    <meta property="og:title" content="Free games and programs for android" />
+    <meta property="og:title" content="AppsPages : Make Your App's Page Online" />
     <meta property="og:description" content="Free Games and Programs, you can download for free, a lot popular games with MOD for Android." />
     <link rel="canonical" href="https://appspages.online/" />
     <meta name="HandheldFriendly" content="true" />
@@ -22,8 +22,8 @@
     <meta name="apple-mobile-web-app-capable" content="yes" />
     <meta name="apple-mobile-web-app-status-bar-style" content="default" />
     <link rel="apple-touch-icon" sizes="180x180" href="logo.png" />
-    <link rel="icon" type="image/png" sizes="32x32" href="logo.png" />
-    <link rel="icon" type="image/png" sizes="16x16" href="logo.png" />
+    <link rel="icon" type="image/png" sizes="32x32" href="https://appspages.online/logo.png" />
+    <link rel="icon" type="image/png" sizes="16x16" href="https://appspages.online/logo.png" />
     <meta name="theme-color" content="#ffffff" />
     <meta name="msapplication-config" content="https://an1.com/templates/an1/images/favicon/browserconfig.xml" />
     <link rel="manifest" href="https://an1.com/templates/an1/images/favicon/manifest.json" />
