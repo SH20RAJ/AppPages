015ba57886bfe7214729c07a6e2eb06be7f9752e
Sh Raj <66713844+SH20RAJ@users.noreply.github.com>
2023-07-01 11:27:06 +0530

    Update app.php

--- app.php
+++ app.php
@@ -52,7 +52,7 @@ if(isset($_GET['p'])){
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <meta http-equiv="X-UA-Compatible" content="ie=edge">
       <title><?php echo $appName ;?> - Download - AppsPages</title>
-      <link rel="stylesheet" href="https://an1.com/templates/an1/css/styles.css?v=6.0">
+      <link rel="stylesheet" href="../../site.css">
      <link rel="icon" href="<?php echo $appIcon;?>"/>
    </head>
    <body>
