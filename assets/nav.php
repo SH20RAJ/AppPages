3052a51000c9fed412d75a21f1539192677e2de1
Sh Raj <66713844+SH20RAJ@users.noreply.github.com>
2023-07-03 18:29:57 +0530

    g

--- assets/nav.php
+++ assets/nav.php
@@ -339,7 +339,7 @@ $menuItems = [
           <button id="qsearch_btn" class="qsearch tool-btn" aria-label="Search" onclick="document.getElementById('html').classList.add('qs');document.getElementById('qsearch_modal').classList.add('open');">
             <svg xmlns="http://www.w3.org/2000/svg" class="ionicon" viewBox="0 0 512 512"><path d="M221.09 64a157.09 157.09 0 10157.09 157.09A157.1 157.1 0 00221.09 64z" fill="none" stroke="currentColor" stroke-miterlimit="10" stroke-width="32"/><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-miterlimit="10" stroke-width="32" d="M338.29 338.29L448 448"/></svg>
           </button>
-          <a href="cpanel/" class="lang xsmf fbold" title="Russian">
+          <a href="../../cpanel/" class="lang xsmf fbold" title="Russian">
             <span class="minbtn">
               <i class="flag" style="background-image: url('https://cdn.icon-icons.com/icons2/506/PNG/512/cloud_icon-icons.com_49310.png');"></i> Dashboard </span>
           </a>
