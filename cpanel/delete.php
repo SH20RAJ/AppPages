885c0e5bc12471266936add1ffcc223004a5a2fe
Sh Raj <66713844+SH20RAJ@users.noreply.github.com>
2023-07-03 20:12:21 +0530

    vv

--- cpanel/delete.php
+++ cpanel/delete.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 include '../conn.php';
 // Check if the app_id is provided in the POST request
 if (isset($_POST['app_id'])) {
@@ -10,7 +11,7 @@ if (isset($_POST['app_id'])) {
 
     if ($conn->query($sql) === TRUE) {
         echo "App deleted successfully.";
-        //header('Location : ../cpanel/?p=apps');
+        header('Location : ../cpanel/?p=apps');
     } else {
         echo "Error deleting app: " . $conn->error;
     }
