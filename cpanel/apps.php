b2aa3668be510010ffe7164c2dc7608795f4d839
Sh Raj <66713844+SH20RAJ@users.noreply.github.com>
2023-07-03 20:03:29 +0530

    -f -a

--- cpanel/apps.php
+++ cpanel/apps.php
@@ -1,58 +1,109 @@
 <?php
+// Initialize variables for storing form data
+$appId = $appName = $appDescription = $screenshots = $appURL = $appCategory = $supportedPlatforms = $appIcon = $appSize = $appVersion = '';
 
-// Prepare and execute the query
-$uploaderId = $_SESSION['uploader_id'];
-$sql = "SELECT app_id, app_name, app_icon, upload_time , views , downloads FROM apps WHERE uploader_id = ?";
-$stmt = $conn->prepare($sql);
-$stmt->bind_param("i", $uploaderId);
-$stmt->execute();
-$result = $stmt->get_result();
+// Check if the form is submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Retrieve the form data
+    $appId = $_POST["app_id"];
+    $appName = $_POST["app_name"];
+    $appDescription = $_POST["app_description"];
+    $screenshots = $_POST["screenshots"];
+    $appURL = $_POST["app_url"];
+    $appCategory = $_POST["app_category"];
+    $supportedPlatforms = $_POST["supported_platforms"];
+    $appIcon = $_POST["app_icon"];
+    $appSize = $_POST["app_size"];
+    $appVersion = $_POST["app_version"];
+    $uploaderId = $_SESSION['uploader_id'];
 
-// Check if any rows are returned
-if ($result->num_rows > 0) {
-    // Output data in a table with CSS styling
-    echo '<table>';
-    echo '<tr>';
-    echo '<th>App Name</th>';
-    echo '<th>App Icon</th>';
-    echo '<th>Upload Date</th>';
-    echo '<th>Edit</th>';
-    echo '<th>Views</th>';
-    echo '<th>Downloads</th>';
-    echo '</tr>';
-
-    // Loop through each row
-    while ($row = $result->fetch_assoc()) {
-        // Access the app information
-        $appid = $row["app_id"];
-        $appName = $row["app_name"];
-        $appIcon = $row["app_icon"];
-        $uploadDate = $row["upload_time"];
-        $views = $row["views"];
-        $downloads = $row["downloads"];
-
-
-        // Display the app details in the table
-        echo '<tr>';
-        echo '<td><img src="' . $appIcon . '" alt="App Icon" width="50" height="50"></td>';
-        echo '<td>' . $appName . '</td>';
-        echo '<td>' . $uploadDate . '</td>';
-        echo '<td><a href="?p=edit&app_id=' . $appid . '">Edit</a></td>';
-        echo '<td>' . $views . '</td>';
-        echo '<td>' . $downloads . '</td>';
-        echo '</tr>';
+    // Update the app details in the database
+    $sql = "UPDATE apps SET app_name='$appName', app_description='$appDescription', screenshots='$screenshots', app_url='$appURL', app_category='$appCategory', supported_platforms='$supportedPlatforms', app_icon='$appIcon', app_size='$appSize', app_version='$appVersion' WHERE app_id='$appId' AND uploader_id='$uploaderId'";
+    
+    if ($conn->query($sql) === TRUE) {
+        echo "App details updated successfully.";
+    } else {
+        echo "Error updating app details: " . $conn->error;
     }
-
-    echo '</table>';
-} else {
-    echo "No apps found.";
 }
 
-// Close the prepared statement and database connection
-$stmt->close();
+// Check if the app_id parameter is provided in the URL
+if (isset($_GET['app_id'])) {
+    $appId = $_GET['app_id'];
+    $uploaderId = $_SESSION['uploader_id'];
+
+    // Retrieve the details of the app from the database
+    $sql = "SELECT app_name, app_description, screenshots, app_url, app_category, supported_platforms, app_icon, app_size, app_version FROM apps WHERE app_id='$appId' AND uploader_id='$uploaderId'";
+    $result = $conn->query($sql);
+
+    // Check if the app exists
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+
+        // Access the app information
+        $appName = $row["app_name"];
+        $appDescription = $row["app_description"];
+        $screenshots = $row["screenshots"];
+        $appURL = $row["app_url"];
+        $appCategory = $row["app_category"];
+        $supportedPlatforms = $row["supported_platforms"];
+        $appIcon = $row["app_icon"];
+        $appSize = $row["app_size"];
+        $appVersion = $row["app_version"];
+
+        // Display a form to edit the app details
+        echo '
+        <form method="post" action="">
+            <label>App ID:</label>
+            <input type="text" name="app_id" value="' . $appId . '" readonly><br><br>
+            
+            <label>App Name:</label>
+            <input type="text" name="app_name" value="' . $appName . '"><br><br>
+            
+            <label>App Description:</label>
+            <textarea name="app_description">' . $appDescription . '</textarea><br><br>
+            
+            <label>Screenshots:</label>
+            <textarea name="screenshots">' . $screenshots . '</textarea><br><br>
+            
+            <label>App URL:</label>
+            <input type="text" name="app_url" value="' . $appURL . '"><br><br>
+            
+            <label>App Category:</label>
+            <input type="text" name="app_category" value="' . $appCategory . '"><br><br>
+            
+            <label>Supported Platforms:</label>
+            <input type="text" name="supported_platforms" value="' . $supportedPlatforms . '"><br><br>
+            
+            <label>App Icon:</label>
+            <input type="text" name="app_icon" value="' . $appIcon . '"><br><br>
+            
+            <label>App Size:</label>
+            <input type="text" name="app_size" value="' . $appSize . '"><br><br>
+            
+            <label>App Version:</label>
+            <input type="text" name="app_version" value="' . $appVersion . '"><br><br>
+            
+            <input type="submit" value="Update">
+        </form>
+        ';
+        
+        // Delete App button
+        echo '<br>
+        <form method="post" action="delete.php">
+            <input type="hidden" name="app_id" value="' . $appId . '">
+            <input type="submit" value="Delete App">
+        </form>';
+    } else {
+        echo "App not found.";
+    }
+}
+
+// Close the database connection
 $conn->close();
 
 ?>
+
 <style>
     table {
         width: 100%;
