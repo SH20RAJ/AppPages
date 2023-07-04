c76dda6bd67e89563f111a5d9ec914ad75fe6c95
Sh Raj <66713844+SH20RAJ@users.noreply.github.com>
2023-07-04 19:33:24 +0530

    4et

--- assets/loaddbapps.php
+++ assets/loaddbapps.php
@@ -6,13 +6,71 @@
       <div class="app_list">
         <?php
         // Retrieve the latest 20 uploaded apps data from the "apps" table
-        $sql = "SELECT * FROM apps where uploader_id = 2 ";
+        echo $sql = "SELECT * FROM apps where uploader_id = " . $user . " ";
         $result = $conn->query($sql);
 
         if ($result->num_rows > 0) {
             // Loop through the result and fetch app data
             foreach ($result as $row) {
-                echo $row['app_name'].'<br/>';
+                $appName = $row['app_name'];
+                $appCategory = $row['app_category'];
+                $appDescription = $row['app_description'];
+                $appVersion = $row['app_version'];
+                $appIcon = $row['app_icon'];
+                $appSize = $row['app_size'];
+                $packageName = $row['package_name'];
+                $screenshots = $row['screenshots'];
+                $appUrl = $row['app_url'];
+
+                // Use heredoc syntax to define the HTML code
+                $html = <<<HTML
+<div class="item">
+  <div class="item_app mod">
+    <div class="img">
+      <img src="$appIcon" alt="$appName" loading="lazy">
+    </div>
+    <div class="cont">
+      <div class="data">
+        <div class="name">
+          <a href="view?p=$packageName" title="$appName">
+            <span>$appName</span>
+          </a>
+        </div>
+        <div class="developer xsmf muted">Max Games Studios</div>
+      </div>
+      <div class="meta muted">
+        <div class="rate_star">
+          <div id="ratig-layer-3381">
+            <div class="rating">
+              <ul class="unit-rating">
+                <li class="current-rating" style="width:72%;">3.6</li>
+                <li>
+                  <a href="#" title="Useless" class="r1-unit" onclick="doRate('1', '3381'); return false;">1</a>
+                </li>
+                <li>
+                  <a href="#" title="Poor" class="r2-unit" onclick="doRate('2', '3381'); return false;">2</a>
+                </li>
+                <li>
+                  <a href="#" title="Fair" class="r3-unit" onclick="doRate('3', '3381'); return false;">3</a>
+                </li>
+                <li>
+                  <a href="#" title="Good" class="r4-unit" onclick="doRate('4', '3381'); return false;">4</a>
+                </li>
+                <li>
+                  <a href="#" title="Excellent" class="r5-unit" onclick="doRate('5', '3381'); return false;">5</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+HTML;
+
+                // Output the HTML code
+                echo $html;
             }
         } else {
             echo "No apps found.";
--- user.php
+++ user.php
@@ -4,22 +4,23 @@ include "conn.php";
 
 
 
- // echo '<!DOCTYPE html><html lang="en" id="html" class="ap-open hfix" prefix="og: https://ogp.me/ns#">';
+  echo '<!DOCTYPE html><html lang="en" id="html" class="ap-open hfix" prefix="og: https://ogp.me/ns#">';
 
-//include "assets/head.php";
+include "assets/head.php";
 
- // echo "<body>";
+ echo "<body>";
 
-//include "assets/nav.php";
-//include "assets/search.php";
+include "assets/nav.php";
+include "assets/search.php";
 
 
 //loadcate($c,ucfirst($c));
+$user = $_GET['user'];
 include 'assets/loaddbapps.php';
 
 
 //include 'assets/loaddbapps.php';
-//include 'assets/footer.php'; 
+include 'assets/footer.php'; 
 ?>
 
   </body>
