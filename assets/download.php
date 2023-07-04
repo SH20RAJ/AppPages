37f7c0900c50932329f1ac71ae006f6eec90d231
Sh Raj <66713844+SH20RAJ@users.noreply.github.com>
2023-07-04 20:08:24 +0530

    uu

--- assets/download.php
+++ assets/download.php
@@ -2,8 +2,11 @@
 include '../conn.php';
 $id = $_GET['id'];
 $url = urldecode($_GET['url']);
-$sql = "UPDATE apps SET downloads = downloads + 1 where package_name='".$_GET['id']."' or app_id='".$_GET['id']."'";
-if($conn->query($sql)){
-    header('Location : '.$url.');
-} 
+$sql = "UPDATE apps SET downloads = downloads + 1 WHERE package_name='" . $id . "' OR app_id='" . $id . "'";
+if ($conn->query($sql)) {
+    header('Location: ' . $url);
+    exit(); // It's a good practice to include an exit statement after a header redirect
+} else {
+    echo "Error updating downloads count: " . $conn->error;
+}
 ?>
