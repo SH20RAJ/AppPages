4a6339796e32d704d9a8487c8df2df34193fa227
Sh Raj <66713844+SH20RAJ@users.noreply.github.com>
2023-07-04 19:32:04 +0530

    t5

--- user.php
+++ user.php
@@ -1,22 +1,21 @@
 <?php
 include "conn.php";
 
-function loaduserapps($user){
-include 'assets/loaddbapps.php';
-}
 
-  echo '<!DOCTYPE html><html lang="en" id="html" class="ap-open hfix" prefix="og: https://ogp.me/ns#">';
+
+
+ // echo '<!DOCTYPE html><html lang="en" id="html" class="ap-open hfix" prefix="og: https://ogp.me/ns#">';
 
 //include "assets/head.php";
 
-  echo "<body>";
+ // echo "<body>";
 
 //include "assets/nav.php";
 //include "assets/search.php";
 
 
 //loadcate($c,ucfirst($c));
-loaduserapps($_GET['user']);
+include 'assets/loaddbapps.php';
 
 
 //include 'assets/loaddbapps.php';
