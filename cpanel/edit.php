7ecf4d6d05f482e83cc7a441f1b8552fb28b048e
Sh Raj <66713844+SH20RAJ@users.noreply.github.com>
2023-07-04 20:35:09 +0530

    msg

--- cpanel/edit.php
+++ cpanel/edit.php
@@ -94,12 +94,15 @@ if (isset($_GET['app_id'])) {
         
         // Delete App button
         echo '<br>
+        
+        <div class="other"><a href="../../view/'.$appId.'"><button style="background:blue;">View</button></a></div>
+        <hr>
+        <p style='color: red;'>Caution: Deleting this app will permanently remove it from the system. This action cannot be undone. Are you sure you want to proceed?</p>
+
         <form method="post" action="delete.php">
             <input type="hidden" name="app_id" value="' . $appId . '">
             <input style="background:red;" type="submit" value="Delete App">
         </form>
-        <div class="other"><a href="../../view/'.$appId.'"><button style="background:blue;">View</button></a></div>
-
         
         
         ';
