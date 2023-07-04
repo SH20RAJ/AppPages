9cc418723f3300c7159127ed324887422faee500
Sh Raj <66713844+SH20RAJ@users.noreply.github.com>
2023-07-04 20:55:26 +0530

    re

--- cpanel/nav.php
+++ cpanel/nav.php
@@ -8,7 +8,7 @@
           <i class="bx bx-search"></i>
         </div>
         <div class="profile-details">
-          <img src="images/profile.jpg" alt="">
+          <img src="https://cdn.iconscout.com/icon/free/png-512/free-avatar-370-456322.png?f=avif&w=256" alt="">
           <span class="admin_name"><?php echo $_SESSION['username'] ;?></span>
           <i class="bx bx-chevron-down"></i>
         </div>
