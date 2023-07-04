3357b14e2f0d3fd7c4848856a4909820a6008478
Sh Raj <66713844+SH20RAJ@users.noreply.github.com>
2023-07-04 19:27:41 +0530

    1

--- assets/loaddbapps.php
+++ assets/loaddbapps.php
@@ -1,24 +1,18 @@
 <hr hint="popular-apps">
 <div class="page">
   <div class="wrp">
-    <h1><?php 
-    include '../conn.php';
-    
-    $user = 2; echo $user; ?></h1>
+    <h1><?php include '../conn.php'; $user = 2; echo $user; ?></h1>
     <div class="content">
       <div class="app_list">
         <?php
-        // Retrieve the latest uploaded apps data from the "apps" table
-        $sql = "SELECT * FROM apps WHERE uploader_id = " . $user . "";
+        // Retrieve the latest 20 uploaded apps data from the "apps" table
+        $sql = "SELECT * FROM apps where uploader_id = " . $user . " ORDER BY upload_time DESC";
         $result = $conn->query($sql);
 
         if ($result->num_rows > 0) {
             // Loop through the result and fetch app data
-            while ($row = $result->fetch_assoc()) {
-                foreach ($row as $key => $value) {
-                    echo $key . ": " . $value . "<br>";
-                }
-                echo "<br>";
+            foreach ($result as $row) {
+                echo $row['app_name'];
             }
         } else {
             echo "No apps found.";
