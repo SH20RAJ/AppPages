495774a19de8a517b1de28d4b73425c2ed77ac97
Sh Raj <66713844+SH20RAJ@users.noreply.github.com>
2023-07-04 19:05:32 +0530

    o9

--- assets/loaddbapps.php
+++ assets/loaddbapps.php
@@ -1,29 +1,29 @@
 <hr hint="popular-apps">
-    <div class="page">
-      <div class="wrp">
-        <h1><?php$user = 2; echo $user; ?></h1>
-        <div class="content">
-          <div class="app_list">
-<?php
-// Retrieve the latest 20 uploaded apps data from the "apps" table
-echo $sql = "SELECT * FROM apps where uploader_id = " . $user . " ORDER BY upload_time DESC";
-$result = $conn->query($sql);
+<div class="page">
+  <div class="wrp">
+    <h1><?php $user = 2; echo $user; ?></h1>
+    <div class="content">
+      <div class="app_list">
+        <?php
+        // Retrieve the latest 20 uploaded apps data from the "apps" table
+        $sql = "SELECT * FROM apps where uploader_id = " . $user . " ORDER BY upload_time DESC";
+        $result = $conn->query($sql);
 
-if ($result->num_rows > 0) {
-    // Loop through the result and fetch app data
-    while ($row = $result->fetch_assoc()) {
-        echo $appName = $row['app_name'];
-        $appCategory = $row['app_category'];
-        $appDescription = $row['app_description'];
-        $appVersion = $row['app_version'];
-        $appIcon = $row['app_icon'];
-        $appSize = $row['app_size'];
-        $packageName = $row['package_name'];
-        $screenshots = $row['screenshots'];
-        $appUrl = $row['app_url'];
+        if ($result->num_rows > 0) {
+            // Loop through the result and fetch app data
+            while ($row = $result->fetch_assoc()) {
+                $appName = $row['app_name'];
+                $appCategory = $row['app_category'];
+                $appDescription = $row['app_description'];
+                $appVersion = $row['app_version'];
+                $appIcon = $row['app_icon'];
+                $appSize = $row['app_size'];
+                $packageName = $row['package_name'];
+                $screenshots = $row['screenshots'];
+                $appUrl = $row['app_url'];
 
-              // Use heredoc syntax to define the HTML code
-        //$html = <<<HTML
+                // Use heredoc syntax to define the HTML code
+                $html = <<<HTML
 <div class="item">
   <div class="item_app mod">
     <div class="img">
@@ -69,18 +69,14 @@ if ($result->num_rows > 0) {
 </div>
 HTML;
 
-        // Output the HTML code
-        echo $html;
-    }
-} else {
-    echo "No apps found.";
-}
-
-?>
-            
-
-            <!--<div class="navigation open" id="navcollapse"><div class="collapse"><div class="navigation_ext"><span><svg class="i__arrowleft"><use xlink:href="#i__arrowleft"></use></svg><span class="vhide">Back</span></span><div class="pages"><span>1</span><a href="https://an1.com/games/page/2/">2</a><a href="https://an1.com/games/page/3/">3</a><a href="https://an1.com/games/page/4/">4</a><span class="nav_ext">...</span><a href="https://an1.com/games/page/258/">258</a></div><a href="https://an1.com/games/page/2/"><svg class="i__arrowright"><use xlink:href="#i__arrowright"></use></svg><span class="vhide">Next</span></a></div></div><div class="navigation_in"><div id="ajax-next-page" class="nav_more"><a href="https://an1.com/games/page/2/"><span class="btn btn-dark"><span class="uppercase fbold smf">More...</span></span></a></div><button id="navcollapse_btn" class="btn uppercase smf" type="button"><span class="fbold">Page 1</span> of 258 <i class="arrow_drop"></i></button></div></div>-->
-          </div>
-        </div>
+                // Output the HTML code
+                echo $html;
+            }
+        } else {
+            echo "No apps found.";
+        }
+        ?>
       </div>
     </div>
+  </div>
+</div>
