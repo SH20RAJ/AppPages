a5639667e3c97b1203996aca40a14421d21e461a
Sh Raj <66713844+SH20RAJ@users.noreply.github.com>
2023-07-04 13:10:18 +0530

    ff

--- api/get.php
+++ api/get.php
@@ -1,4 +1,8 @@
 <?php
+// Disable CORS and allow cross-origin requests
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
 
 // Create a cURL resource
 $curl = curl_init();
