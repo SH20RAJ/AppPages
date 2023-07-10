c2b2b3331705cc0f932dc06cbf8798f47b947074
Sh Raj <66713844+SH20RAJ@users.noreply.github.com>
2023-07-10 09:47:14 +0530

    ew

--- assets/loaddbapps.php
+++ assets/loaddbapps.php
@@ -9,7 +9,7 @@
       <div class="app_list">
         <?php
         // Retrieve the latest 20 uploaded apps data from the "apps" table
-        $sql = "SELECT * FROM apps where uploader_id = " . $user . " ";
+        $sql = "SELECT * FROM apps where uploader_id = " . $user . " ORDER BY app_id DESC";
         $result = $conn->query($sql);
 
         if ($result->num_rows > 0) {
