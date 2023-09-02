2b5cb5c0b7b67f4800f21eeb3a25f70cf261299f
Sh Raj <66713844+SH20RAJ@users.noreply.github.com>
2023-09-02 08:37:22 +0530

    download.php

---
+++ download.php
@@ -0,0 +1,31 @@
+<?php
+include 'conn.php';
+// Fetch app data from the "apps" table
+$sql = "SELECT * FROM apps where package_name='".$_GET['p']."' or app_id='".$_GET['p']."' ";
+$result = $conn->query($sql);
+
+function bytesToMB($bytes) {
+    $megabytes = round($bytes / 1024 / 1024 , 1);
+    return $megabytes;
+}
+
+
+if ($result->num_rows > 0) {
+
+        $appName = $row["app_name"];
+        $appCategory = $row["app_category"];
+        $appDesc = $row["app_description"];
+        $appVersion = $row["app_version"];
+        $appIcon = $row["app_icon"];
+        $appSize = bytesToMB($row["app_size"]);
+        $packageName = $row["package_name"];
+        $screenshots = explode(",", $row["screenshots"]);
+        $appUrl = '../../assets/download.php?url='.urlencode($row["app_url"]).'&id='.$row["app_id"];
+
+        header("Location: $appUrl");
+
+} else {
+    echo "No App Found";
+}
+
+
