6e8ff6b2a7bf1a37fbee66e6d8decdd9ff341f42
Sh Raj <shaswatraj3@gmail.com>
2024-03-30 13:03:36 +0530

    set hero sec

--- assets/main_hero.php
+++ assets/main_hero.php
@@ -4,7 +4,7 @@
           <h2 class="title">Games catalog</h2>
           <p>In our catalog you can download the latest versions of popular games for Android</p>
           <a class="btn btn-green" href="/games/">
-            <span class="uppercase fbold smf">All Games</span>
+            <!-- <span class="uppercase fbold smf">All Games</span> -->
           </a>
           <a class="btn btn-orange" style="display: none;" href="/tags/mods/">
             <span class="uppercase fbold smf">MOD Games</span>
@@ -16,7 +16,7 @@
           </h2>
           <p>Free catalog of the latest MOD versions of games that you can download for free</p>
           <a class="btn btn-dark" href="/tags/mods/">
-            <span class="uppercase fbold smf">All MOD-games</span>
+            <!-- <span class="uppercase fbold smf">All MOD-games</span> -->
           </a>
         </div>
       </div>
