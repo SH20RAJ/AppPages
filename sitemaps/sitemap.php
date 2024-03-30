0ad4ba61c60ba81c505f1e5b71a187ab83b9cab3
Sh Raj <shaswatraj3@gmail.com>
2024-03-30 09:23:54 +0530

    Update sitemap.php

--- sitemaps/sitemap.php
+++ sitemaps/sitemap.php
@@ -1,43 +1,49 @@
 <?php
-// Fetch JSON data from the URL
-$url = 'https://ws2-cache.aptoide.com/api/7/listApps?limit=10&offset=2&sort=trending60d';
-$json = file_get_contents($url);
+// Fetch JSON data from the URLs
+$urls = [
+    'https://ws2-cache.aptoide.com/api/7/listApps?limit=1000&offset=2&sort=trending60d',
+    'https://ws2-cache.aptoide.com/api/7/listApps?limit=1000&offset=300&sort=trending60d',
+    'https://ws2-cache.aptoide.com/api/7/listApps?limit=1000&offset=4&sort=trending60d'
+];
 
-// Decode JSON data
-$data = json_decode($json, true);
+$allData = [];
 
-// Check if data was successfully retrieved
-if ($data && isset($data['datalist']['list']) && !empty($data['datalist']['list'])) {
-    // Start building the sitemap
-    $xml = '<?xml version="1.0" encoding="UTF-8"?>
+foreach ($urls as $url) {
+    $json = file_get_contents($url);
+    $data = json_decode($json, true);
+
+    // Check if data was successfully retrieved
+    if ($data && isset($data['datalist']['list']) && !empty($data['datalist']['list'])) {
+        $allData = array_merge($allData, $data['datalist']['list']);
+    }
+}
+
+// Start building the sitemap
+$xml = '<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
 
-    // Iterate through each item in the list
-    foreach ($data['datalist']['list'] as $item) {
-        $uname = $item['uname'];
-        $loc = "https://appspages.online/app/$uname";
+// Iterate through each item in the combined list
+foreach ($allData as $item) {
+    $uname = $item['uname'];
+    $loc = "https://appspages.online/app/$uname";
 
-        // Add URL to the sitemap
-        $xml .= '
+    // Add URL to the sitemap
+    $xml .= '
     <url>
         <loc>' . htmlspecialchars($loc) . '</loc>
         <lastmod>' . date('c') . '</lastmod>
         <changefreq>daily</changefreq>
         <priority>0.8</priority>
     </url>';
-    }
+}
 
-    // End the sitemap
-    $xml .= '
+// End the sitemap
+$xml .= '
 </urlset>';
 
-    // Set the correct content type
-    header('Content-Type: application/xml');
+// Set the correct content type
+header('Content-Type: application/xml');
 
-    // Output the XML
-    echo $xml;
-} else {
-    // Handle error if data is empty or not in expected format
-    echo "Error: Unable to fetch or process data.";
-}
+// Output the XML
+echo $xml;
 ?>
